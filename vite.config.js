fbea0f1fee069d145ec4715d7dfde1cbc29cc4d4
mxrph1n3 <d3sxmxrphine@gmail.com>
2026-03-17 17:32:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Если вы используете новый Tailwind
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  // ЭТА СТРОКА КРИТИЧЕСКИ ВАЖНА:
-  base: '/alpha-star/', 
+  plugins: [react()],
+  base: '/alpha-star/',
 })
